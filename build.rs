418d7248f75c8094afb90b41a742bbc8ce210079
Riven Skaye <riven@tae.moe>
2024-09-30 14:50:50 +0200

    Make builds play nice on docs.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
-#[cfg(target_os = "windows")]
+#[cfg(any(target_os = "windows", docsrs))]
 pub fn main() {
     println!("cargo::rustc-cfg=windows_slim_errors")
 }
 
-#[cfg(not(target_os = "windows"))]
+#[cfg(all(not(docsrs), not(target_os = "windows")))]
 pub fn main() {
     compile_error!("This crate is literally made for the Windows API. Please learn how conditional compilation works.")
 }
