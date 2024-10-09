cd2ae4daa111cbf34a281e0d466b52fcc93afebe
Riven Skaye <riven@tae.moe>
2024-10-09 09:42:07 +0200

    Clean up build script, change unreachable compile error to very reachable panic

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
-#[cfg(any(target_os = "windows", docsrs))]
 pub fn main() {
-    println!("cargo::rustc-cfg=windows_slim_errors")
-}
-
-#[cfg(all(not(docsrs), not(target_os = "windows")))]
-pub fn main() {
-    compile_error!("This crate is literally made for the Windows API. Please learn how conditional compilation works.")
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() || std::env::var_os("DOCS_RS").is_some() {
+        println!("cargo::rustc-cfg=windows_slim_errors")
+    } else {
+        panic!("This crate only works for Windows targets. Please disable usage and references on other OSes.")
+    }
 }
