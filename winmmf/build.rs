0cd348c0c0d99386669e433c4581534d129b169a
Riven Skaye <riven@tae.moe>
2024-10-10 15:15:16 +0200

    List the source

--- winmmf/build.rs
+++ winmmf/build.rs
@@ -2,6 +2,6 @@ pub fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() || std::env::var_os("DOCS_RS").is_some() {
         println!("cargo::rustc-cfg=windows_slim_errors")
     } else {
-        panic!("This crate only works for Windows targets. Please disable usage and references on other OSes.")
+        panic!("WinMMF: This crate only works for Windows targets. Please disable usage and references on other OSes.")
     }
 }
