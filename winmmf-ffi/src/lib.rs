24a27edfc0e878925357c6550b1f44a8c6f28b3c
Riven Skaye <riven@tae.moe>
2024-10-11 15:40:00 +0200

    non-violent ffi crimes

--- winmmf-ffi/src/lib.rs
+++ winmmf-ffi/src/lib.rs
@@ -1,36 +1,96 @@
 #![feature(let_chains)]
 
+use ffi_support::FfiStr;
 use std::{
-    ffi::{c_char, CStr},
-    num::{NonZeroU32, NonZeroUsize},
+    num::NonZeroUsize,
     sync::{Mutex, OnceLock},
 };
+pub use winmmf::Namespace as ValidNamespaces;
 use winmmf::{states::RWLock, *};
 
+/// You didn't think I was foing to keep _this_ long a type unaliased right?
 type MMFWrapper<'a> = Mutex<Vec<MemoryMappedFile<RWLock<'a>>>>;
+
+/// A wrapper to hold any MMFs that are produced during the application lifetime.
 static MMFS: OnceLock<MMFWrapper> = OnceLock::new();
 
+/// Lazy wrapper to use when ensuring initialization
 fn _init<'a>(cap: usize) -> MMFWrapper<'a> {
     Mutex::new(Vec::with_capacity(cap))
 }
 
-pub extern "system" fn init(count: Option<NonZeroU32>) -> u32 {
-    let cap = count.map(|c| c.get()).unwrap_or(1) as usize;
+/// Initialize the inner object to hold MMF instances.
+///
+/// Returns: 0 on success, 1 on error.
+/// The only conceivable error state would be calling this function more than once.
+#[no_mangle]
+pub extern "system" fn init(count: Option<NonZeroUsize>) -> usize {
+    let cap = count.map(|c| c.get()).unwrap_or(1);
     MMFS.set(_init(cap)).map(|_| 0).unwrap_or(1)
 }
 
-pub extern "system" fn open(size: u32, name: *const c_char, namespace: u8) -> u32 {
-    if let Ok(ns) = namespace.try_into()
-        && let Some(size) = NonZeroUsize::new(size as usize)
-        && let Ok(namestr) = unsafe { CStr::from_ptr(name) }.to_str()
-        && let Ok(mapped) = MemoryMappedFile::new(size, namestr, ns)
-    {
-        if MMFS.get_or_init(|| _init(1)).lock().map(|mut inner| inner.push(mapped)).is_ok() {
-            0
-        } else {
-            2
+/// Open an existing MMF and push it into the list.
+///
+/// The caller is responsible for tracking current indices when more than one MMF is opened.
+///
+/// There are several possible return values here, these are:
+/// 0: Success
+/// 1: Size is 0
+/// 2: The name is invalid UTF-8
+/// 3: The namespace is invalid
+/// 4: The MMF could not be opened
+/// 5: The MMF could not be stored
+#[no_mangle]
+pub extern "system" fn open(size: Option<NonZeroUsize>, name: FfiStr, namespace: u8) -> usize {
+    match (size, name.as_opt_str(), namespace.try_into()) {
+        (None, _, _) => 1,
+        (_, None, _) => 2,
+        (_, _, Err(_)) => 3,
+        (Some(size), Some(namestr), Ok(ns)) => {
+            if let Ok(mapped) = MemoryMappedFile::open(size, namestr, ns) {
+                MMFS.get_or_init(|| _init(1))
+                    .lock()
+                    .map(|mut inner| {
+                        inner.push(mapped);
+                        0
+                    })
+                    .unwrap_or(5)
+            } else {
+                4
+            }
         }
-    } else {
-        1
+    }
+}
+
+/// Create a new MMF and push it into the list.
+///
+/// The caller is responsible for tracking current indices when more than one MMF is opened.
+///
+/// There are several possible return values here, these are:
+/// 0: Success
+/// 1: Size is 0
+/// 2: The name is invalid UTF-8
+/// 3: The namespace is invalid
+/// 4: The MMF could not be opened
+/// 5: The MMF could not be stored
+#[no_mangle]
+pub extern "system" fn new(size: Option<NonZeroUsize>, name: FfiStr, namespace: u8) -> usize {
+    match (size, name.as_opt_str(), namespace.try_into()) {
+        (None, _, _) => 1,
+        (_, None, _) => 2,
+        (_, _, Err(_)) => 3,
+        (Some(size), Some(namestr), Ok(ns)) => {
+            if let Ok(mapped) = MemoryMappedFile::new(size, namestr, ns) {
+                MMFS.get_or_init(|| _init(1))
+                    .lock()
+                    .map(|mut inner| {
+                        inner.push(mapped);
+                        0
+                    })
+                    .unwrap_or(5)
+            } else {
+                4
+            }
+        }
     }
 }
