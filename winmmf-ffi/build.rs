347b9f57bee3c68b2c20a5a55c5845fe1158a1ba
Riven Skaye <riven@tae.moe>
2024-10-15 12:15:52 +0200

    chore: appease clippy

--- winmmf-ffi/build.rs
+++ winmmf-ffi/build.rs
@@ -1,8 +1,7 @@
 pub fn main() {
     if !(std::env::var_os("CARGO_CFG_WINDOWS").is_some() || std::env::var_os("DOCS_RS").is_some()) {
         println!(
-            "cargo::warning={}",
-            "WinMMF-ffi: This crate only works for Windows targets. Please disable usage and references on other OSes.\nNothing is guaranteed to work when cross-compiling!"
+            "cargo::warning=WinMMF-ffi: This crate only works for Windows targets. Please disable usage and references on other OSes.\nNothing is guaranteed to work when cross-compiling!"
         )
     }
     println!("cargo::rustc-cfg=windows_slim_errors")
